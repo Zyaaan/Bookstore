ca9246b15dfedf0c6476a50a5aacacd2b2714ba1
Zyaaan <96587918+Zyaaan@users.noreply.github.com>
2023-12-19 01:07:27 +0800

    Update BookDao.java

--- src/model/BookDao.java
+++ src/model/BookDao.java
@@ -1,7 +1,24 @@
+/**
+ * The BookDao interface defines methods for accessing book-related data from a data source.
+ * Implementations of this interface should provide functionality to retrieve book information.
+ * It follows Java coding conventions for readability and maintainability.
+ * 
+ * @author [Telmen]
+ * @version 1.0
+ * @since [2023-11-22]
+ */
+
 package model;
 
 import java.sql.SQLException;
 
 public interface BookDao {
+
+    /**
+     * Retrieves book data from the data source.
+     *
+     * @throws SQLException if there is an issue with the data source.
+     */
     public void getDataBooks() throws SQLException;
+
 }
