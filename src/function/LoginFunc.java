aaa0a376a7b39ceb1748477a36c5c3aa8ba6fcea
Zyaaan <96587918+Zyaaan@users.noreply.github.com>
2023-12-19 01:03:34 +0800

    Update LoginFunc.java

--- src/function/LoginFunc.java
+++ src/function/LoginFunc.java
@@ -1,4 +1,15 @@
+/**
+ * The LoginFunc class provides methods for checking login credentials and registering new users.
+ * It interacts with the Main class, User class, and Login view to perform authentication and registration tasks.
+ * The class follows Java coding conventions for readability and maintainability.
+ * 
+ * @author [Enkhzaya]
+ * @version 1.0
+ * @since [2023-11-30]
+ */
+
 package function;
+
 import Controller.Main;
 import model.User;
 import view.Login;
@@ -8,27 +19,39 @@ import java.sql.SQLException;
 
 public class LoginFunc {
     // Constructor for LoginFunc class
-    public LoginFunc(){
-
+    public LoginFunc() {
+        // Constructor may be used for future enhancements
     }
 
-    // Method to check login credentials
-    public void loginCheck(String mail, String pass){
+    /**
+     * Method to check login credentials.
+     *
+     * @param mail The email entered by the user.
+     * @param pass The password entered by the user.
+     */
+    public void loginCheck(String mail, String pass) {
         // Check if the provided email exists in the userLogin map
-        if(Main.userLogin.containsKey(mail)){
+        if (Main.userLogin.containsKey(mail)) {
             // If email exists, check if the provided password matches the stored password
-            if(Main.userLogin.get(mail).getPassword().toString().equals(pass)){
+            if (Main.userLogin.get(mail).getPassword().toString().equals(pass)) {
                 // Set the logged-in user in the Login class and update the login flag
                 Login.A = Main.userLogin.get(mail);
                 Login.LoginFlag = 1; // Set login flag to indicate successful login
-            };
+            }
         }
     }
 
-    // Method to check and register a new user
-    public void registerCheck(String pass1, String pass2, String name, String mail){
+    /**
+     * Method to check and register a new user.
+     *
+     * @param pass1 The first entered password.
+     * @param pass2 The second entered password for confirmation.
+     * @param name  The name of the new user.
+     * @param mail  The email of the new user.
+     */
+    public void registerCheck(String pass1, String pass2, String name, String mail) {
         // Check if the entered passwords match
-        if(pass1.equals(pass2)){
+        if (pass1.equals(pass2)) {
             // Create a new User object with the provided details
             User newUser = new User(name, mail, pass1, 0);
 
@@ -46,3 +69,4 @@ public class LoginFunc {
         }
     }
 }
+
