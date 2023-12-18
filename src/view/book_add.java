65f94bdc6b1a161a43ed2838c4a88f138c3fb2b6
Zyaaan <96587918+Zyaaan@users.noreply.github.com>
2023-12-19 01:24:12 +0800

    Update book_add.java

--- src/view/book_add.java
+++ src/view/book_add.java
@@ -11,6 +11,7 @@ import java.awt.event.KeyEvent;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+// Class for adding or editing books
 public class book_add {
     private JPanel panel1;
     private JTextField textField1;
@@ -22,20 +23,26 @@ public class book_add {
     private JTextPane textArea1;
     private JButton save;
 
+    // Constructor
     public book_add() {
-        if(AdminHome.editFlag==1){
+        // Populate fields if editing an existing book
+        if (AdminHome.editFlag == 1) {
             textField1.setText(AdminHome.B.getCategory());
             textField2.setText(AdminHome.B.getName());
             textField3.setText(AdminHome.B.getAuthor());
-            textField4.setText(""+ AdminHome.B.getPrice());
-            textField5.setText(""+ AdminHome.B.getNumber());
+            textField4.setText("" + AdminHome.B.getPrice());
+            textField5.setText("" + AdminHome.B.getNumber());
             textArea1.setText(AdminHome.B.getDesc());
         }
+
+        // Set up the UI frame
         JFrame u = new JFrame();
         u.setContentPane(panel1);
         u.setTitle("НОМ");
         u.setSize(720, 480);
         u.setVisible(true);
+
+        // Restrict input for price and number fields to numeric values
         textField4.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -48,6 +55,7 @@ public class book_add {
                 }
             }
         });
+
         textField5.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -60,10 +68,14 @@ public class book_add {
                 }
             }
         });
+
+        // Save button action listener
         save.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(AdminHome.editFlag==1){
+                // Check if editing an existing book
+                if (AdminHome.editFlag == 1) {
+                    // Update existing book in the database
                     String query = "UPDATE book SET name = ?, category = ?, author = ?, price = ?, number = ?, description = ? WHERE id = ?";
                     DatabaseConnection dbConnection = null;
                     try {
@@ -86,7 +98,8 @@ public class book_add {
                     } catch (SQLException ex) {
                         throw new RuntimeException(ex);
                     }
-                }else{
+                } else {
+                    // Insert new book into the database
                     String query = "INSERT INTO book (name, category, author, price, number, description) VALUES (?, ?, ?, ?, ?, ?)";
                     DatabaseConnection dbConnection = null;
                     try {
